8752d58f0747de08fc402b93375151af39d19f16
BertrandGB <bertrand.gb@gmail.com>
2018-12-10 11:17:57 +0100

    r0.6 : JS verification for navbar hiding

--- js/index.js
+++ js/index.js
@@ -6,18 +6,43 @@ console.log("JS is started");
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+  // Select all links with hashes
+  // standard code from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+          &&
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
@@ -44,11 +69,13 @@ console.log("JS is started");
   $(window).scroll(navbarCollapse);
 
   // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
+  $('.menus-modal').on('show.bs.modal', function(e) {
     $('.navbar').addClass('d-none');
+    console.log("add");
   })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+  $('.menus-modal').on('hidden.bs.modal', function(e) {
     $('.navbar').removeClass('d-none');
+    console.log("remove");
   })
 
 })(jQuery); // End of use strict
