61f54a3b4767e816c923aa83d37bfd8c21cf8c76
BertrandGB <bertrand.gb@gmail.com>
2018-12-12 09:19:37 +0100

    r0.6.a : include jQuery part1

--- js/index.js
+++ js/index.js
@@ -5,6 +5,23 @@ console.log("JS is started");
 (function($) {
   "use strict"; // Start of use strict
 
+
+
+  var a = {};
+
+  $.getJSON('data.json', function(data) {
+      a = data;
+      console.log(a);
+      $.each(a, function(idx, elem) {
+          console.log(idx);
+          console.log(elem);
+          $('table#tbl tbody').append('<tr><td>' + elem.ID + '</td><td>' + elem.Name + '</td><td>' + elem.IDNumber + '</td></tr>');
+      });
+  });
+
+
+
+
   // Smooth scrolling using jQuery easing
   // Select all links with hashes
   // standard code from https://css-tricks.com/snippets/jquery/smooth-scrolling/
